5b9b10a37d0a36fb87fc79d6fe871b442c4171e1
dipak-DEV-3055 <dipak.duple@gmail.com>
2023-12-13 18:31:07 +0530

    Remove unused routes.

--- routes/expertRoutes.js
+++ routes/expertRoutes.js
@@ -13,13 +13,5 @@ const { createUser, updateUser, changePassword } = require('../controllers/exper
 router.post('/add',isAuthenticate,checkTransactionPassword,validator(CreateExpertValidate),createUser);
 router.post('/update',isAuthenticate,checkTransactionPassword,validator(UpdateExpertValidate),updateUser);
 router.post('/password',isAuthenticate,checkTransactionPassword,validator(changePasswordExpertValidate),changePassword);
-// // router.post('/lockUnlockUser', validator(LockUnlockUser), lockUnlockUser);
-// router.post('/changePassword',isAuthenticate,checkTransactionPassword,validator(ChangePassword),changePassword);
-// router.post('/lockUnlockUser',isAuthenticate, checkTransactionPassword,validator(LockUnlockUser), lockUnlockSuperAdmin);
-// // router.post('/changePassword',isAuthenticate,validator(ChangePassword),changePassword);
-// router.post("/update/exposurelimit",isAuthenticate,checkTransactionPassword,validator(setExposureLimitValid),setExposureLimit)
-// router.post("/update/creditreferrence",isAuthenticate,checkTransactionPassword,validator(setCreditReferValid),setCreditReferrence)
-// router.post("/update/balance",isAuthenticate,checkTransactionPassword,validator(SetSuperAdminBalance),updateUserBalance)
 
 module.exports = router;
-//https://3100dev.fairgame.club/fair-game-wallet/getUserBalanceDetails

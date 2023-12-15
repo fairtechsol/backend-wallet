c2817b09833cfaeaf76c5b392edd3929a0026a93
nain4884 <nain4884@gmail.com>
2023-12-15 17:04:44 +0530

    add the logger

--- config/logger.js
+++ config/logger.js
@@ -6,38 +6,71 @@ const errorTransport = new DailyRotateFile({
   level: 'error',
   filename: 'logs/error-%DATE%.log',
   datePattern: 'YYYY-MM-DD',
-  zippedArchive: true,
-  maxSize: '20m',
-  maxFiles: '14d'
+  maxFiles: '20d'
 });
 
 const infoTransport = new DailyRotateFile({
   level: 'info',
   filename: 'logs/info-%DATE%.log',
   datePattern: 'YYYY-MM-DD',
-  zippedArchive: true,
-  maxSize: '20m',
-  maxFiles: '14d'
+  maxFiles: '20d'
 });
 
 const debugTransport = new DailyRotateFile({
   level: 'debug',
   filename: 'logs/debug-%DATE%.log',
   datePattern: 'YYYY-MM-DD',
-  zippedArchive: true,
-  maxSize: '20m',
-  maxFiles: '14d'
+  maxFiles: '20d'
+});
+
+const combineTransport = new DailyRotateFile({
+  level: 'silly',
+  filename: 'logs/combine-%DATE%.log',
+  datePattern: 'YYYY-MM-DD',
+  maxFiles: '20d'
 });
 
 // Create a logger and add transports
-const logger = winston.createLogger({
+const infoLogger = winston.createLogger({
+  format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
   transports: [
-    new winston.transports.Console(),
-    errorTransport,
     infoTransport,
-    debugTransport
-    // Add other transports for different log levels as needed
+    combineTransport
   ]
 });
 
-exports.LOGGER = logger
+const errorLogger = winston.createLogger({
+  format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+  transports: [
+    errorTransport,
+    combineTransport
+  ]
+});
+
+const debugLogger = winston.createLogger({
+  format: winston.format.combine(winston.format.timestamp(), winston.format.json()),
+  transports: [
+    debugTransport,
+    combineTransport
+  ]
+});
+
+if (process.env.NODE_ENV != 'prod') {
+  infoLogger.add(new winston.transports.Console());
+  errorLogger.add(new winston.transports.Console());
+  debugLogger.add(new winston.transports.Console());
+}
+
+const logger = {
+  info: (params) => {
+    return infoLogger.info(params);
+  },
+  error: (params) => {
+    return errorLogger.error(params);
+  },
+  debug: (params) => {
+    return debugLogger.debug(params);
+  },
+};
+
+exports.logger = logger

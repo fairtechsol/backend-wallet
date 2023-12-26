c833d48d27a629f31ff65aa3710d6f53299e0e4d
dipak-DEV-3055 <dipak.duple@gmail.com>
2023-12-26 18:05:14 +0530

    add password field in redis

--- config/internalRedisConnection.js
+++ config/internalRedisConnection.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const internalRedis = new Redis({
     host: process.env.INTERNAL_REDIS_HOST || 'localhost',
     port: process.env.INTERNAL_REDIS_PORT || 6379,
+    password : process.env.INTERNAL_REDIS_PASSWORD || ''
 });
 
 // Listen for the 'connect' event

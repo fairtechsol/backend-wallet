5ae23fb569d2330cde9874284460cdc2ebad6131
rohanduple <150104513+rohanduple@users.noreply.github.com>
2024-02-08 17:02:34 +0530

    match lock

--- routes/matchRoutes.js
+++ routes/matchRoutes.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 const { isAuthenticate } = require('../middleware/auth');
-const { matchDetails, listMatch, addMatch } = require('../controllers/matchController');
+const { matchDetails, listMatch, addMatch, matchLock } = require('../controllers/matchController');
 
 
 
 
 router.get('/list',isAuthenticate,listMatch);
 router.get('/:id',isAuthenticate,matchDetails);
+router.post('/lock',isAuthenticate,matchLock);
 router.post('/add', addMatch);
 
 module.exports = router;

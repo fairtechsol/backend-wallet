21b8deadabb63debe383a6780ebb7124678df01b
rohanduple <150104513+rohanduple@users.noreply.github.com>
2024-02-12 15:19:28 +0530

    bug fixes betplace

--- queue/consumer.js
+++ queue/consumer.js
@@ -90,11 +90,11 @@ let calculateRateAmount = async (jobData, userId) => {
           let masterRedisData = await getUserRedisData(partnershipId);
           if (lodash.isEmpty(masterRedisData)) {
             let partnerUser = await getUserBalanceDataByUserId(partnershipId);
-            let partnerExposure = partnerUser.exposure - userOldExposure + userCurrentExposure;
+            let partnerExposure = (partnerUser?.exposure || 0) - userOldExposure + userCurrentExposure;
             await updateUserBalanceByUserId(partnershipId, { exposure: partnerExposure });
           } else {
             let masterExposure = masterRedisData.exposure ? masterRedisData.exposure : 0;
-            let partnerExposure = masterExposure - userOldExposure + userCurrentExposure;
+            let partnerExposure = (masterExposure || 0) - userOldExposure + userCurrentExposure;
             await updateUserBalanceByUserId(partnershipId, { exposure: partnerExposure });
 
             let teamRates = {
@@ -205,14 +205,14 @@ const calculateSessionRateAmount = async (jobData, userId) => {
           if (lodash.isEmpty(masterRedisData)) {
             // If masterRedisData is empty, update partner exposure
             let partnerUser = await getUserBalanceDataByUserId(partnershipId);
-            let partnerExposure = partnerUser.exposure - maxLossExposure;
+            let partnerExposure = (partnerUser.exposure || 0) - maxLossExposure;
             await updateUserBalanceByUserId(partnershipId, {
               exposure: partnerExposure,
             });
           } else {
             // If masterRedisData exists, update partner exposure and session data
             let masterExposure = parseFloat(masterRedisData.exposure) ?? 0;
-            let partnerExposure = masterExposure + maxLossExposure;
+            let partnerExposure = (masterExposure || 0) + maxLossExposure;
             await updateUserBalanceByUserId(partnershipId, {
               exposure: partnerExposure,
             });
@@ -321,14 +321,14 @@ walletSessionBetDeleteQueue.process((job, done) => {
             if (lodash.isEmpty(masterRedisData)) {
               // If masterRedisData is empty, update partner exposure
               let partnerUser = await getUserBalanceDataByUserId(partnershipId);
-              let partnerExposure = partnerUser.exposure - exposureDiff;
+              let partnerExposure = (partnerUser.exposure || 0) - exposureDiff;
               await updateUserBalanceByUserId(partnershipId, {
                 exposure: partnerExposure,
               });
             } else {
               // If masterRedisData exists, update partner exposure and session data
               let masterExposure = parseFloat(masterRedisData.exposure) ?? 0;
-              let partnerExposure = masterExposure - exposureDiff;
+              let partnerExposure = (masterExposure || 0) - exposureDiff;
               await updateUserBalanceByUserId(partnershipId, {
                 exposure: partnerExposure,
               });
@@ -450,14 +450,14 @@ walletMatchBetDeleteQueue.process((job, done) => {
             if (lodash.isEmpty(masterRedisData)) {
               // If masterRedisData is empty, update partner exposure
               let partnerUser = await getUserBalanceDataByUserId(partnershipId);
-              let partnerExposure = partnerUser.exposure - exposureDiff;
+              let partnerExposure = (partnerUser.exposure || 0) - exposureDiff;
               await updateUserBalanceByUserId(partnershipId, {
                 exposure: partnerExposure,
               });
             } else {
               // If masterRedisData exists, update partner exposure and session data
               let masterExposure = parseFloat(masterRedisData.exposure) ?? 0;
-              let partnerExposure = masterExposure - exposureDiff;
+              let partnerExposure = (masterExposure || 0) - exposureDiff;
               await updateUserBalanceByUserId(partnershipId, {
                 exposure: partnerExposure,
               });

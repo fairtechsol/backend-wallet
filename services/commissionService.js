72bf734758e49eaa770afe10580639ed0ec1f3f8
rohanduple <150104513+rohanduple@users.noreply.github.com>
2024-03-13 10:01:53 +0530

    fixed get commission for settled

--- services/commissionService.js
+++ services/commissionService.js
@@ -1,3 +1,4 @@
+const { Not, IsNull } = require("typeorm");
 const { AppDataSource } = require("../config/postGresConnection");
 const commissionSchema = require("../models/commission.entity");
 const Commission = AppDataSource.getRepository(commissionSchema);
@@ -15,7 +16,7 @@ exports.deleteCommission = (betId) => {
 }
 
 exports.commissionReport = (userId, query) => {
-  const commissionMatches = Commission.createQueryBuilder().where({ parentId: userId , settled : false }).groupBy('"matchId"').addGroupBy(`"matchName"`).addGroupBy(`"matchStartDate"`).select(['commission.matchName as "matchName"', 'commission.matchId as "matchId"', 'commission.matchStartDate as "matchStartDate"','ROUND((Sum(commission.commissionAmount * commission.partnerShip)  / 100)::numeric, 2) as amount'])
+  const commissionMatches = Commission.createQueryBuilder().where({ parentId: userId , matchId : Not(IsNull()) }).groupBy('"matchId"').addGroupBy(`"matchName"`).addGroupBy(`"matchStartDate"`).select(['commission.matchName as "matchName"', 'commission.matchId as "matchId"', 'commission.matchStartDate as "matchStartDate"','ROUND((Sum(commission.commissionAmount * commission.partnerShip)  / 100)::numeric, 2) as amount'])
 
   if (query.page) {
     commissionMatches.skip((parseInt(query.page) - 1) * parseInt(query.limit || 10)).take(parseInt(query?.limit || 10));
@@ -24,7 +25,7 @@ exports.commissionReport = (userId, query) => {
 }
 
 exports.commissionMatchReport = (userId, matchId) => {
-  const commissionMatches = Commission.createQueryBuilder().where({ parentId: userId, matchId: matchId }).select(['commission.userName as "userName"', 'commission.teamName as "name"', 'commission.betPlaceDate as "date"', 'commission.teamName as "teamName"', 'commission.odds as "odds"', 'commission.betType as "betType"', 'commission.stake as "stake"', 'commission.commissionAmount as "commissionAmount"','commission.commissionType as "commissionType"','commission.partnerShip as "partnerShip"','commission.matchName as "matchName"']);
+  const commissionMatches = Commission.createQueryBuilder().where({ parentId: userId, matchId: matchId }).select(['commission.userName as "userName"', 'commission.teamName as "name"', 'commission.betPlaceDate as "date"', 'commission.teamName as "teamName"', 'commission.odds as "odds"', 'commission.betType as "betType"', 'commission.stake as "stake"', 'commission.commissionAmount as "commissionAmount"','commission.commissionType as "commissionType"','commission.partnerShip as "partnerShip"','commission.matchName as "matchName"','commission.settled as "settled"']);
 
   return commissionMatches.getRawMany();
 }

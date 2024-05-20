0cefb2922808859d4e48d4ab515852345d28052f
GauravDuple <gauravmishra.duple@gmail.com>
2024-05-20 20:25:12 +0530

    create match api changes

--- routes/matchRoutes.js
+++ routes/matchRoutes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
+<<<<<<< Updated upstream
 const { isAuthenticate, checkTransactionPassword } = require('../middleware/auth');
 const { matchDetails, listMatch, addMatch, matchLock, otherMatchDetails, checkChildDeactivate } = require('../controllers/matchController');
+=======
+const { isAuthenticate } = require('../middleware/auth');
+const { matchDetails, listMatch, addMatch, matchLock, otherMatchDetails, raceAdd } = require('../controllers/matchController');
+>>>>>>> Stashed changes
 
 
 
@@ -13,5 +18,6 @@ router.get('/other/:id', isAuthenticate, otherMatchDetails);
 router.post('/lock', isAuthenticate, checkTransactionPassword, matchLock);
 router.get("/checkChildDeactivate", isAuthenticate, checkChildDeactivate);
 router.post('/add', addMatch);
+router.post('/raceAdd', raceAdd);
 
 module.exports = router;

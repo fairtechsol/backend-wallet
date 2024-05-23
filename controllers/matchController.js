ce19e8208f03bec71ba84bd01487706e0534b12c
GauravDuple <gauravmishra.duple@gmail.com>
2024-05-23 18:02:54 +0530

    match details i18 changes

--- controllers/matchController.js
+++ controllers/matchController.js
@@ -103,7 +103,7 @@ exports.raceDetails = async (req, res) => {
     return SuccessResponse(
       {
         statusCode: 200,
-        message: { msg: "match details", keys: { name: "Match" } },
+        message: { msg: "match.matchDetails", keys: { name: "Match" } },
         data: apiResponse.data
       },
       req,

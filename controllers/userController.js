11d659f1045c693d49fc1e299e1e53b9f7fa2327
nain4884 <nain4884@gmail.com>
2024-06-27 18:02:41 +0530

    update the list of user partnership show wrong for user in pop-up

--- controllers/userController.js
+++ controllers/userController.js
@@ -542,7 +542,7 @@ exports.userList = async (req, res, next) => {
       );
     }
 
-    let userRole = reqUser.roleName;
+    let userRole = roleName || reqUser.roleName;
     let where = {
       createBy: userId || reqUser.id,
     };

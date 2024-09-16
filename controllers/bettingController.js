b88d0cebdc3ce58c2718e28548b80b0e19ebe84f
rohanduple <150104513+rohanduple@users.noreply.github.com>
2024-09-16 12:28:26 +0530

    Added delete bet for tournament

--- controllers/bettingController.js
+++ controllers/bettingController.js
@@ -172,59 +172,6 @@ exports.deleteMultipleBetForRace = async (req, res) => {
     }
 };
 
-exports.deleteMultipleBetForTournament = async (req, res) => {
-    try {
-        let { matchId, deleteReason, urlData } = req.body;
-        let domain = expertDomain;
-        let matchExist = {};
-        try {
-            matchExist = await apiCall(
-                apiMethod.get,
-                domain + allApiRoutes.MATCHES.matchDetails + matchId
-            );
-            if (!matchExist) {
-                return ErrorResponse(
-                    { statusCode: 404, message: { msg: "notFound", keys: { name: "Match" } } },
-                    req,
-                    res
-                );
-            }
-        } catch (error) {
-            throw error?.response?.data;
-        }
-        if (!Object.keys(urlData).length) {
-            return ErrorResponse({ statusCode: 400, message: { msg: "invalid", keys: { name: "url data" } } }, req, res);
-        }
-        let promiseArray = []
-        for (let url in urlData) {
-            let promise = apiCall(
-                apiMethod.post,
-                url + allApiRoutes.deleteMultipleBetForTournament,
-                { data: urlData[url], deleteReason, matchId }
-            );
-            promiseArray.push(promise);
-        }
-        let failedUrl = new Set();
-        await Promise.allSettled(promiseArray)
-            .then(results => {
-                let urlDataArray = Object.keys(urlData);
-                results.forEach((result, index) => {
-                    if (result.status === 'rejected') {
-                        failedUrl.add(urlDataArray[index]);
-                    }
-                });
-            })
-            .catch(error => {
-                console.error('Error in handling settled promises:', error);
-            });
-        if (failedUrl.size) {
-            return ErrorResponse({ statusCode: 400, message: { msg: "deleteBetError", keys: { urlData: Array.from(failedUrl).join(', ') } } }, req, res);
-        }
-        return SuccessResponse({ statusCode: 200, message: { msg: "updated", keys: { name: "Bet" } } }, req, res);
-    } catch (err) {
-        return ErrorResponse(err, req, res);
-    }
-};
 exports.changeBetsDeleteReason = async (req, res) => {
     try {
         let { deleteReason, betData, matchId } = req.body;
--- routes/betRoutes.js
+++ routes/betRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { deleteMultipleBet, getSessionProfitLoss,deleteMultipleBetForOther, deleteMultipleBetForRace, changeBetsDeleteReason, deleteMultipleBetForTournament } = require('../controllers/bettingController.js');
+const { deleteMultipleBet, getSessionProfitLoss,deleteMultipleBetForOther, deleteMultipleBetForRace, changeBetsDeleteReason } = require('../controllers/bettingController.js');
 
 const validator = require('../middleware/joi.validator');
 const { isAuthenticate } = require('../middleware/auth');
@@ -10,7 +10,6 @@ const { deleteMultipleBetValidator, changeBetsDeleteReasonValidator } = require(
 router.post('/deleteMultipleBet', isAuthenticate, validator(deleteMultipleBetValidator), deleteMultipleBet);
 router.post('/deleteMultipleBetForOther', isAuthenticate, validator(deleteMultipleBetValidator), deleteMultipleBetForOther);
 router.post('/deleteMultipleBetForRace', isAuthenticate, validator(deleteMultipleBetValidator), deleteMultipleBetForRace);
-router.post('/deleteMultipleBetForTournament', isAuthenticate, validator(deleteMultipleBetValidator), deleteMultipleBetForTournament);
 router.post('/change/deleteReason', isAuthenticate, validator(changeBetsDeleteReasonValidator), changeBetsDeleteReason);
 router.get('/session/profitLoss/:betId', isAuthenticate, getSessionProfitLoss);
 
